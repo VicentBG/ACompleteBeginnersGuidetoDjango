0adc9289d6a0ec4cdb3e9baa97db90d01bec7dad
Vicente Bataller <vbatallerg@gmail.com>
2018-12-05 16:55:53 +0100

    Modificado models y templates

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,7 +1,10 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Board
 
 # Create your views here.
 
 def home(request):
-    return HttpResponse('Hello World!')
+    boards = Board.objects.all()
+    
+    return render(request, 'home.html', {'boards': boards})
