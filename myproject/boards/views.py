1695f92acaeada4bec57b0e306c727967ebb6de6
Vicente Bataller <vbatallerg@gmail.com>
2018-12-12 15:53:13 +0100

    Creacion de urls y reusable templates

--- myproject/boards/views.py
+++ myproject/boards/views.py
@@ -1,5 +1,5 @@
-from django.shortcuts import render
-from django.http import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
 from .models import Board
 
 # Create your views here.
@@ -8,3 +8,8 @@ def home(request):
     boards = Board.objects.all()
     
     return render(request, 'home.html', {'boards': boards})
+
+def board_topics(request, pk):
+    board = get_object_or_404(Board, pk=pk)
+    return render(request, 'topics.html', {'board': board})
+
--- myproject/myproject/urls.py
+++ myproject/myproject/urls.py
@@ -13,12 +13,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
+
 from django.conf.urls import url
+from django.contrib import admin
 
 from boards import views
 
 urlpatterns = [
     url(r'^$', views.home, name='home'),
+    url(r'^boards/(?P<pk>\d+)/$', views.board_topics, name='board_topics'),
     url(r'^admin/', admin.site.urls),
 ]
