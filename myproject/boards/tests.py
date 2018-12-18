22104b0ef245afdf59f6a8e2497afb161ee470d6
VicentBG <vbatallerg@gmail.com>
2018-12-18 15:48:23 +0100

    Mejoras en forms y algun test mas

--- myproject/boards/tests.py
+++ myproject/boards/tests.py
@@ -3,6 +3,7 @@ from django.test import TestCase
 from django.contrib.auth.models import User
 from .views import home, board_topics, new_topic
 from .models import Board, Topic, Post
+from .forms import NewTopicForm
 
 # Create your tests here.
 
@@ -98,7 +99,9 @@ class NewTopicTests(TestCase):
         '''
         url = reverse('new_topic', kwargs={'pk': 1})
         response = self.client.post(url, {})
+        form = response.context.get('form')
         self.assertEquals(response.status_code, 200)
+        self.assertTrue(form.errors)
 
     def test_new_topic_invalid_post_data_empty_fields(self):
         url = reverse('new_topic', kwargs={'pk': 1})
@@ -111,4 +114,9 @@ class NewTopicTests(TestCase):
         self.assertFalse(Topic.objects.exists())
         self.assertFalse(Post.objects.exists())
 
-    
+    def test_contains_form(self):
+        url = reverse('new_topic', kwargs={'pk': 1})
+        response = self.client.get(url)
+        form = response.context.get('form')
+        self.assertIsInstance(form, NewTopicForm)
+
