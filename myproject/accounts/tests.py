5ba02191db7f306f59b35198f397ec15254526b7
VicentBG <vbatallerg@gmail.com>
2018-12-20 16:43:54 +0100

    Desarrollo de la pagina del registro y mas tests

--- myproject/accounts/tests.py
+++ myproject/accounts/tests.py
@@ -1,16 +1,75 @@
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
 from django.urls import reverse, resolve
 from django.test import TestCase
-from django.contrib.auth.models import User
 from .views import signup
 
 # Create your tests here.
 
 class SignUpTests(TestCase):
-    def test_signup_status_code(self):
+    def setUp(self):
         url = reverse('signup')
-        response = self.client.get(url)
-        self.assertEqual(response.status_code, 200)
+        self.response = self.client.get(url)
+
+    def test_signup_status_code(self):
+        self.assertEqual(self.response.status_code, 200)
 
     def test_signup_url_resolves_signup_view(self):
         view = resolve('/signup/')
         self.assertEqual(view.func, signup)
+
+    def test_csrf(self):
+        self.assertContains(self.response, 'csrfmiddlewaretoken')
+
+    def test_contains_form(self):
+        form = self.response.context.get('form')
+        self.assertIsInstance(form, UserCreationForm)
+
+class SuccessfulSignUpTests(TestCase):
+    def setUp(self):
+        url = reverse('signup')
+        data = {
+            'username': 'Juan',
+            'password1': 'abcdef123456',
+            'password2': 'abcdef123456'
+        }
+        self.response = self.client.post(url, data)
+        self.home_url = reverse('home')
+
+    def test_redirection(self):
+        '''
+        Un formulario cumplimentado de manera correcta debe redireccionar al usuario a home.
+        '''
+        self.assertRedirects(self.response, self.home_url)
+
+    def test_user_creation(self):
+        self.assertTrue(User.objects.exists())
+
+    def test_user_authentication(self):
+        '''
+        Crear petición a página aleatoria.
+        La respuesta resultante debe tener un usario en su contexto,
+        después de un registro correcto.
+        '''
+        response = self.client.get(self.home_url)
+        user = response.context.get('user')
+        self.assertTrue(user.is_authenticated)
+
+class InvalidSignUpTests(TestCase):
+    def setUp(self):
+        url = reverse('signup')
+        self.response = self.client.post(url, {}) # Se envía un diccionario vacío.
+
+    def test_signup_status_code(self):
+        '''
+        Un formulario inválido debe retornar a la misma página.
+        '''
+        self.assertEqual(self.response.status_code, 200)
+
+    def test_form_errors(self):
+        form = self.response.context.get('form')
+        self.assertTrue(form.errors)
+
+    def test_dont_create_user(self):
+        self.assertFalse(User.objects.exists())
+
